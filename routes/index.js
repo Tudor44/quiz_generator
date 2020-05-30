5a85cd325024700e31ade8256bc699f57cb360e8
Gaetano D'Orsi <olimpusdrs@gmail.com>
2020-05-30 09:46:04 +0200

    Added author page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+var router = express.Router();	
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+/* GET home page. */	/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Quiz' });	
 });
+
+// Author page.
+router.get('/author', (req, res, next) => {
+    res.render('author');
+});
+
 
 module.exports = router;
