dfa95bf8fef46d811c03abf0b1ddd27d86e94f2c
Gaetano D'Orsi <olimpusdrs@gmail.com>
2020-05-30 12:43:35 +0200

    Added express-partials

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var partials = require('express-partials');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -14,7 +15,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(partials());
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
